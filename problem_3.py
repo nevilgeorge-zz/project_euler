903a80dd36f022e49c639c56f9d0519cbd8a46c6
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-10-31 20:29:41 -0500

    problem 3

---
+++ problem_3.py
@@ -0,0 +1,25 @@
+# Problem 3
+
+import sys
+original = int(sys.argv[1])
+
+
+def checkPrime(value):
+	# check if there is a factor that is not value or 1
+	for i in range(value - 1, 2, -1):
+		if value % i == 0:
+			return False
+
+	return True
+
+def primeFactor(value):
+	# a factor cannot be larger than the root of the input
+	limit = original ** (0.5)
+	limit = int(round(limit))
+	# count downwards from the limit because you want to find the largest one
+	for i in range(limit, 1, -1):
+		if original % i == 0:
+			if checkPrime(i):
+				return i
+
+print primeFactor(original)
